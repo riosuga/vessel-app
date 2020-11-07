aa4ca3a58d43eeadbe545a14273f2a358d83a094
riosuga <riosugacp@gmail.com>
2020-11-07 15:49:12 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,16 @@
 const mysql = require('mysql');
 
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'db_vessel'
+// });
+
 const conn = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'pps1',
+  password: 'PPS1W3b#',
   database: 'db_vessel'
 });
 
